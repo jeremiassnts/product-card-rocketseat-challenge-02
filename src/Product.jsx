601d9c1236229bad85a4c36ae3da04f8c39b0953
Jeremias Santos <jeremias.santos@newcheck.com.br>
2023-09-21 15:42:06 -0300

    feat: states

--- src/Product.jsx
+++ src/Product.jsx
@@ -3,19 +3,19 @@ import couch from './assets/couch.png'
 import close from './assets/close.svg'
 import image360 from './assets/360.svg'
 import couch360 from './assets/couch.gif'
+import { useState } from 'react'
 
 export function Product() {
+    const [show360, setShow360] = useState(false)
     return (
         <div className={styles.container}>
             <div className={styles.productImage}>
-                <img src={couch} alt='Pink couch' />
-                <button>
+                {!show360 ? <img src={couch} alt='Pink couch' /> : <img src={couch360} alt='Pink couch rotating' />}
+                {!show360 ? <button onClick={() => setShow360(true)}>
                     <img src={image360} alt='360 turn icon' />
-                </button>
-                {/* <img src={couch360} alt='Pink couch rotating' /> */}
-                {/* <button>
+                </button> : <button onClick={() => setShow360(false)}>
                     <img src={close} alt='Close icon' />
-                </button> */}
+                </button>}
             </div>
             <div className={styles.info}>
                 <span className={`lato ${styles.code}`}>CÓDIGO: 42404</span>
